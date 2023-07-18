d954bb93327b4eb6c34863cd829ed43bd8243dde
Mostafa Ahmed <mostafaahmed@Mostafas-MacBook-Air.local>
2023-07-19 02:26:26 +0300

    Setting up the files we shall work with

---
+++ lib/messenger_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Messenger_screen extends StatelessWidget {
+  const Messenger_screen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.menu),
+        title: const Text("Messenger"),
+        actions: const [],
+      ),
+      body: const Column(
+        children: [],
+      ),
+    );
+  }
+}
