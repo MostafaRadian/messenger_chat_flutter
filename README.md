0397e24509970b98995f584c3a51c23ece40ba34
Mostafa Ahmed <mostafaahmed@Mostafas-MacBook-Air.local>
2023-07-19 02:16:25 +0300

    Main project files

---
+++ README.md
@@ -0,0 +1,3 @@
+# messenger_chat_page
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/messenger_chat_page/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.messenger_chat_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
