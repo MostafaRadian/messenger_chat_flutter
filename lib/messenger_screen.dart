3ff4a4ef9b038905573878f52240462c0374bce4
MostafaRadian <mostafa.batesta@gmail.com>
2023-07-20 19:31:43 +0300

    setting up the appbar

--- lib/messenger_screen.dart
+++ lib/messenger_screen.dart
@@ -6,15 +6,38 @@ class Messenger_screen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
-        leading: const Image(
-            image: NetworkImage(
-                "https://icons-for-free.com/iconfiles/icns/512/user+icon-1320190636314922883.icns")),
-        title: const Text("Messenger"),
-        actions: const [],
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        leading: const CircleAvatar(
+          radius: 20,
+          backgroundImage: NetworkImage(
+              "https://i.pinimg.com/originals/fd/14/a4/fd14a484f8e558209f0c2a94bc36b855.png"),
+        ),
+        title: const Text(
+          "Chats",
+          style: TextStyle(
+              fontSize: 18, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: const CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.blue,
+                child: Icon(
+                  Icons.photo_camera,
+                  size: 25,
+                ),
+              ))
+        ],
       ),
-      body: const Column(
-        children: [],
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
       ),
     );
   }
