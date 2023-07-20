7eddc80722f0702b1507cd9f805ecddccf86b30f
MostafaRadian <mostafa.batesta@gmail.com>
2023-07-20 19:56:48 +0300

    AppBar fully done

--- lib/messenger_screen.dart
+++ lib/messenger_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
-class Messenger_screen extends StatelessWidget {
-  const Messenger_screen({super.key});
+class MessengerScreen extends StatelessWidget {
+  const MessengerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +11,14 @@ class Messenger_screen extends StatelessWidget {
         backgroundColor: Colors.white,
         elevation: 0,
         centerTitle: true,
-        leading: const CircleAvatar(
-          radius: 20,
-          backgroundImage: NetworkImage(
-              "https://i.pinimg.com/originals/fd/14/a4/fd14a484f8e558209f0c2a94bc36b855.png"),
+        leading: Container(
+          padding: const EdgeInsets.all(10),
+          height: 5,
+          child: const CircleAvatar(
+            radius: 15,
+            backgroundImage: NetworkImage(
+                "https://i.pinimg.com/originals/fd/14/a4/fd14a484f8e558209f0c2a94bc36b855.png"),
+          ),
         ),
         title: const Text(
           "Chats",
@@ -27,10 +31,14 @@ class Messenger_screen extends StatelessWidget {
               icon: const CircleAvatar(
                 radius: 20,
                 backgroundColor: Colors.blue,
-                child: Icon(
-                  Icons.photo_camera,
-                  size: 25,
-                ),
+                child: Icon(Icons.photo_camera, size: 18, color: Colors.white),
+              )),
+          IconButton(
+              onPressed: () {},
+              icon: const CircleAvatar(
+                radius: 20,
+                backgroundColor: Colors.blue,
+                child: Icon(Icons.edit, size: 18, color: Colors.white),
               ))
         ],
       ),
